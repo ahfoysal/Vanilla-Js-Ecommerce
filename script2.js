1e04bce101929a1269986381b6e5a3e77fdefa96
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-17 01:05:02 +0600

    jj

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const APIURL2 = 'https://expressbuybd.com/wp-json/wc/v3/products?per_page=100';
+const APIURL2 = 'https://expressbuybd.com/wp-json/wc/v3/products?';
 
 const APIURL =
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
@@ -10,18 +10,30 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const siteUrl = 'https://expressbuybd.com/';
-const per_page = 'per_page=30';
-const categorie = 'category=250';
+const per_page = 'per_page=6';
+// const categorie = 'category=250';
 // initially get fav movies
 getMovies(APIURL2);
+getMovies2(APIURL2);
 
 async function getMovies(url) {
-    const resp = await fetch(url + key);
+    const resp = await fetch(url + per_page + key);
     const respData = await resp.json();
 
     console.log(respData);
 
     showMovies(respData);
+}
+
+async function getMovies2(url) {
+    let categorie = 'category=251';
+
+    const resp = await fetch(url + categorie + key);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies2(respData);
 }
 
 function showMovies(movies) {
@@ -55,13 +67,46 @@ function showMovies(movies) {
               </div>
               <div class="overview">
                   <h3>Categories:${categories[0].name} ${short_description}</h3>
-                  <a href='https://expressbuybd.com/?add-to-cart=${id}'>Buy NOW</a>
+                  <a href='https://expressbuybd.com/?add-to-cart=${id}'><button class="buy-btn">Buy Now</button>
+                  </a>
 
                   
               </div> </div>
         `;
 
         main.appendChild(movieEl);
+    });
+}
+
+function showMovies2(movies) {
+    // clear main
+    // const IMGPATH = images[0].src;
+
+    mains.innerHTML = '';
+    movies.forEach((movie) => {
+        const { images, name, price, permalink, id, short_description } = movie;
+
+        const movieEl = document.createElement('section');
+        movieEl.classList.add('card');
+        // console.log(name, price);
+
+        movieEl.innerHTML = ` 
+        <div class="product-image">
+        <a href="${permalink}">   <img src="${images[0].src}" alt="OFF-white Red Edition" draggable="false" /></a>
+        </div>
+        <div class="product-info">
+            <h2>${name}</h2>
+            <p>${short_description}</p>
+            <div class="price">${price}</div>
+        </div>
+        <div class="btn">
+        <a href='https://expressbuybd.com/?add-to-cart=${id}'><button class="buy-btn">Buy Now</button>
+        </div>
+     
+    
+     `;
+
+        mains.appendChild(movieEl);
     });
 }
 
