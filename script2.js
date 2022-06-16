bb78bb3a8f3dda8a0e141cc26ce19906b6752110
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-16 23:24:58 +0600

    jkxbas

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-const APIURL2 = 'https://expressbuybd.com/wp-json/wc/v3/products?per_page=15';
+const APIURL2 = 'https://expressbuybd.com/wp-json/wc/v3/products?per_page=100';
 
 const APIURL =
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
@@ -11,7 +11,7 @@ const form = document.getElementById('form');
 const search = document.getElementById('search');
 const siteUrl = 'https://expressbuybd.com/';
 const per_page = 'per_page=30';
-
+const categorie = 'category=250';
 // initially get fav movies
 getMovies(APIURL2);
 
@@ -30,13 +30,21 @@ function showMovies(movies) {
 
     main.innerHTML = '';
     movies.forEach((movie) => {
-        const { images, name, price, categories, permalink, id } = movie;
+        const {
+            images,
+            name,
+            price,
+            categories,
+            permalink,
+            id,
+            short_description,
+        } = movie;
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         // console.log(name, price);
 
-        movieEl.innerHTML = `
+        movieEl.innerHTML = `<div class="row">
         <a href="${permalink}"> <img
         src="${images[0].src}"
         alt="${name}"
@@ -46,11 +54,11 @@ function showMovies(movies) {
                 <span class="">${price}</span>
               </div>
               <div class="overview">
-                  <h3>Categories:${categories[0].name} </h3>
-                  <a href='https://expressbuybd.com/?add-to-cart=${id}'>Buy NOW<a/>
+                  <h3>Categories:${categories[0].name} ${short_description}</h3>
+                  <a href='https://expressbuybd.com/?add-to-cart=${id}'>Buy NOW</a>
 
                   
-              </div>
+              </div> </div>
         `;
 
         main.appendChild(movieEl);
@@ -79,3 +87,5 @@ form.addEventListener('submit', (e) => {
         search.value = '';
     }
 });
+
+////slick
