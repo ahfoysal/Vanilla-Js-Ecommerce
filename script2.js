df4a979b3b3fc90abad258f304acc9b3d4797ad2
hosan30 <50867996+hosan30@users.noreply.github.com>
2022-06-17 22:55:06 +0600

    mnsav

--- script2.js
+++ script2.js
@@ -165,3 +165,38 @@ function bal() {
 
     x[0].style.display = 'none';
 }
+
+/////////loder
+// By @AbubakerSaeed96 (Twitter)
+// ===================
+
+// Inspiration:
+// Oleg Frolov's dribbble shot: https://dribbble.com/shots/5331825-Loading-XXI
+
+// I thought making this would be easy ...but boy 😩
+
+// Thank You For Viewing!
+// Say 👋🏻 or hire me 👉🏻 abubaker.saeed.1996@gmail.com
+// ================================================================
+
+// Helper(s)
+// =========
+let $ = (e) => document.querySelector(e);
+
+// Dots
+// ====
+let dots = $('.dots');
+
+// Function
+// ========
+function animate(element, className) {
+    element.classList.add(className);
+    setTimeout(() => {
+        element.classList.remove(className);
+        setTimeout(() => {
+            animate(element, className);
+        }, 500);
+    }, 2500);
+}
+
+animate(dots, 'dots--animate');
